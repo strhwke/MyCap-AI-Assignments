13df56179a9883216e58453ad882250838bbaa11
Archisman Hes <71208805+strhwke@users.noreply.github.com>
2023-04-11 01:57:27 +0530

    Add files via upload

---
+++ Assignment 3 positive numbers.py
@@ -0,0 +1,15 @@
+##program to traverse a list?
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+
+num = 0
+ 
+while(num < len(list1)):
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+    num += 1
+    
+while(num < len(list2)):
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+    num += 1
