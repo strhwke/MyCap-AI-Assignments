6e6ef728194aab7b245c029a1e4865610c172cd2
Archisman Hes <71208805+strhwke@users.noreply.github.com>
2023-04-11 01:40:02 +0530

    Delete Fibonacci.py

--- Fibonacci.py
+++
@@ -1,8 +0,0 @@
-"""Print a Fibonacci series up to n."""
-print("Enter the no. of terms for the fibonacci sequence")
-n = int(input())
-a, b = 0, 1
-while a < n:
-    print(a, end=' ')
-    a, b = b, a + b
-print()
